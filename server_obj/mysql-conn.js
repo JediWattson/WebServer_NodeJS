30ca4c6370e3451ce9ae2d3e4e9dd301f4415e48
Robert Daly <rdaly@hartford.edu>
2017-04-06 23:31:45 -0400

    more testing with pagination plugin

--- server_obj/mysql-conn.js
+++ server_obj/mysql-conn.js
@@ -32,7 +32,7 @@ module.exports = function mysqlconn(t){
 				return this.forge().query({where:{handle: handle}}).fetch();
 			},
 			byRow: function(off){
-				return this.forge().query().fetchPage({limit: 20, offset: off})
+				return this.query().fetchPage({limit: 20, offset: off})
 			},
 			addBlog: function(blog){
 				return this.forge(blog).save()
