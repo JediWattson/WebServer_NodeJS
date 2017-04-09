f3a06c69315f97e37c9f49da12ad1eb8bfb00194
Robert Daly <rdaly@hartford.edu>
2017-04-09 19:59:07 -0400

    debug request to see how to parse

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/addBlog', (req, res)=>{
 })
 
 app.get('/getRows', (req, res)=>{
-	blogMess.byRow(0).then((result)=>{
+	blogMess.getRows().then((result)=>{
 		res.send(result)
 	}).catch((err)=>{throw err})
 })
